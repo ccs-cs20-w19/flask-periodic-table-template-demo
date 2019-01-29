e879720d1f1d6ca4c0c7bcb479e6d458a4258dbf
Phill Conrad <pconrad@cs.ucsb.edu>
2019-01-29 12:17:24 -0800

    try again

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-#import makeWords
+import makeWords
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World! (2)'
 
 #@app.route('/make/<word>')
 #def show_make(word):
--- makeWords.py
+++ makeWords.py
@@ -8,8 +8,6 @@ def printAllWords():
     if ways != set():
       print(ways)
 
-
-
 def countWords():
  allWordCount = 0
  canMakeCount = 0
