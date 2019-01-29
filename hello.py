431ce6ada5cece10488158c52f223cc94229e9d2
Phill Conrad <pconrad@cs.ucsb.edu>
2019-01-29 12:13:01 -0800

    comment out part that might be crashing

--- hello.py
+++ hello.py
@@ -1,15 +1,14 @@
-import makeWords
+#import makeWords
 
 from flask import Flask
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/make/<word>')
-def show_make(word):
-    # show the user profile for that user
-    return repr(makeWords.wordToElems(word))
+#@app.route('/make/<word>')
+#def show_make(word):
+#    # show the user profile for that user
+#    return repr(makeWords.wordToElems(word))
 
