3e476639e89083a796c0689fc22fe3a9b397152d
Phill Conrad <pconrad@cs.ucsb.edu>
2019-02-12 13:46:02 -0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,11 +1,13 @@
 import makeWords
 
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! (3)'
+    return render_template('hello.html')
 
 @app.route('/make/<word>')
 def show_make(word):
